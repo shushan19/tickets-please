9278ee5299050a4800e440959240202ba0f04fa8
shushan19 <pandeyshushan@gmail.com>
2025-04-23 17:12:01 +0545

    conditionally ommeting and including data

--- app/Http/Resources/V1/TicketResource.php
+++ app/Http/Resources/V1/TicketResource.php
@@ -18,6 +18,32 @@ class TicketResource extends JsonResource
         return [
             'type'=> 'ticket',
             'id'=> $this->id,
+            'attributes'=>
+            [
+                'titile'=> $this->title,
+                'description'=> $this->when($request->routeIs('tickets.show'),$this->description),
+                'status'=>$this->status,
+                'createdAt'=>$this->created_at,
+                'updatedAt'=>$this->updated_at,
+            ],
+            'relationships'=>[
+                'author'=>[
+                    'data'=>[
+                        'type' => 'user',
+                        'id'=> $this->user_id,
+                    ],
+                    'links'=>[
+                        ['self'=>'todo']
+                    ]
+
+                ]
+            ],
+            'includes '=>[
+                new UserResource($this->user),
+            ],
+            'links' =>[
+                ['self'=>route('tickets.show',['ticket'=>$this->id])]
+            ]
         ];
     }
 }
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UsersContorller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->apiResource('tickets',TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('users',Userscontorller::class);
 
 
